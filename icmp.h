75f5b1df846c33c5ad3ee18443c7936e063c0169
morita-koki <morita82007koki@gmail.com>
2023-11-11 22:19:56 +0900

    step11: finish

--- icmp.h
+++ icmp.h
@@ -1,7 +1,12 @@
 #ifndef ICMP_H
 #define ICMP_H
 
-#define ICMP_SIZE 8
+#include <stddef.h>
+#include <stdint.h>
+
+#include "ip.h"
+
+#define ICMP_HDR_SIZE 8
 
 #define ICMP_TYPE_ECHOREPLY           0
 #define ICMP_TYPE_DEST_UNREACH        3
@@ -14,6 +19,10 @@
 #define ICMP_TYPE_TIMESTAMPREPLY     14
 #define ICMP_TYPE_INFO_REQUEST       15
 #define ICMP_TYPE_INFO_REPLY         16
+
+
+extern int
+icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst);
 
 extern int
 icmp_init(void);
