40cbf571e0dcee8d0468bdcdfdc74e153f1cd092
morita-koki <morita82007koki@gmail.com>
2023-12-13 14:42:59 +0900

    step15: prepare

--- arp.c
+++ arp.c
@@ -176,6 +176,10 @@ arp_cache_insert(ip_addr_t pa, const uint8_t *ha)
     return cache;
 }
 
+static int
+arp_request(struct net_iface *iface, ip_addr_t tpa)
+{}
+
 
 static int 
 arp_reply(struct net_iface *iface, const uint8_t *tha, ip_addr_t tpa, const uint8_t *dst)
